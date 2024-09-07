eeb3d48b3707ec03a174a9e09f108a397f1287dc
PetAtHomeK9 <franklinadenipekun@gmail.com>
2024-09-07 17:44:41 +0100

    dog_details done

--- shop/admin.py
+++ shop/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from .models import Dog, Order, Profile
+from .models import Dog, Order, SellerProfile
 
 
 # Register your models here.
 
 admin.site.register(Dog)
 admin.site.register(Order)
-admin.site.register(Profile)
+admin.site.register(SellerProfile)
--- shop/models.py
+++ shop/models.py
@@ -33,8 +33,6 @@ class Order(models.Model):
 
 
 
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    phone_number = models.CharField(max_length=15, blank=True, null=True)
-    address = models.CharField(max_length=255, blank=True, null=True)
+
+
 
