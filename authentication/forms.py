fa0379fdd6bdd93d7b3be1255b2da7d6489441d7
joseph miguel <josephchimaobi099@gmail.com>
2024-09-08 01:34:52 +0100

    added profile

--- authentication/forms.py
+++ authentication/forms.py
@@ -1,12 +1,26 @@
 from django import forms
-from .models import User # Profile
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from .models import User
+from django.contrib.auth.forms import UserCreationForm
 
 class SignupForm(UserCreationForm):
-    first_name=forms.CharField(max_length=20, required=True, help_text='First name')
-    last_name=forms.CharField(max_length=20, required=True, help_text='Last name')
-    email=forms.EmailField(max_length=30, required=True, help_text='Email')
-
+    first_name=forms.CharField(max_length=20, required=True, 
+              widget=forms.TextInput({"placeholder":'First name'})
+              )
+    last_name=forms.CharField(max_length=20, required=True,
+              widget=forms.TextInput({'placeholder':'Last name'})
+              )
+    email=forms.EmailField(max_length=30, required=True, 
+              widget=forms.TextInput({'placeholder':'Email'})
+              )
+    username=forms.CharField(max_length=20,required=True, 
+              widget=forms.TextInput({'placeholder':'Username'})
+              )
+    password1=forms.CharField(min_length=8,required=True,
+              widget=forms.PasswordInput({'placeholder':'Password'})
+              )
+    password2=forms.CharField(min_length=8,required=True,
+              widget=forms.PasswordInput({'placeholder':'Confirm Password'})
+              )
     class Meta:
         model= User
         fields= ('first_name','last_name','email','username','password1','password2')
@@ -14,15 +28,6 @@ class SignupForm(UserCreationForm):
 class LoginForm(forms.Form):
     username = forms.CharField(max_length=20, label=None, required=True, widget=forms.TextInput({"placeholder" : "Enter your username"}))
     password=forms.CharField(min_length=8, label= None,required=True, widget=forms.PasswordInput({"placeholder": "Enter your password"}))
-
-# class ProfileForm(forms.ModelForm):
-#     class Meta:
-#         model = Profile
-#         fields= ['bio','profile_img','location','birth_date']
-
-#         widgets={
-#             'birth_date': forms.DateInput(attrs={'type': 'date'})
-#         }
 
 
         
--- authentication/urls.py
+++ authentication/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path
-from .views import log_in,sign_up
+from .views import log_in,sign_up,log_out,profile
 
 
 
 urlpatterns = [
+    path('profile/', profile, name='profile'),
     path('', log_in, name='log_in'),
-    path('sign_up/', sign_up, name='sign_up'),
+  
+    path('signup/', sign_up, name='sign_up'),
+    path('log_out/', log_out, name='log_out'),
+   
+ 
 ]
 
 
