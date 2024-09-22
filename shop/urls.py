08384345b609e44cb6a175dd1b4e9607de093f0d
PetAtHomeK9 <franklinadenipekun@gmail.com>
2024-09-22 02:44:08 +0100

    profile

--- shop/urls.py
+++ shop/urls.py
@@ -7,9 +7,11 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('dog_details/<int:dog_id>/', views.dog_details, name='dog_details'),
     path('place_order/<int:dog_id>/', views.place_order, name='place_order'),
-    path('order_detail/<int:order_id>/', views.order_detail, name='order_detail'),
+    path('order_details/<int:order_id>/', views.order_detail, name='order_details'),
     path('search/', views.search, name='search_results'),
     path('sell_dog/', views.sell_dog, name='sell_dog'),
+    path('profile/', views.profile_view, name='profile'),
+
 ]
 
 
